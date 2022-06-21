19f59541c646cbf5cb630791f01f57fd05e1a7f9
Brian Griffin <wbgriffin@pm.me>
2022-06-21 17:50:35 -0400

    Started base script

---
+++ scripts/base.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ sudo apt update -y
+ sudo apt upgrade -y
+
+ sudo timedatectl set-timezone America/New_York
+
+ sudo apt install -y curl \
+  git \
+  tmux \
+  vim \
+  htop \
+  grc \
+  jq \
+  ifconfig
