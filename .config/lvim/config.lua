8bb93fd473a06e574d1801a16e00e95d5bf85ebb
Brian Griffin <wbgriffin@pm.me>
2023-05-08 21:13:16 -0400

    Add lunarvim config

---
+++ .config/lvim/config.lua
@@ -0,0 +1,18 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.format_on_save = true
+
+-- =====================================================================
+-- = Keybindings
+-- =====================================================================
+
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+-- Exit insert mode
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
