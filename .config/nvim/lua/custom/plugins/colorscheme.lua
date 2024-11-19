096738f957e399c09177ba4e7200d48b512bbf2a
Brian Griffin <wbgriffin@pm.me>
2024-11-19 10:55:54 -0500

    switch nvim colorscheme -> tokyodark

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,5 +1,18 @@
+-- return {
+--   'scottmckendry/cyberdream.nvim',
+--   lazy = false,
+--   priority = 1000,
+-- }
+--
+
 return {
-  'scottmckendry/cyberdream.nvim',
-  lazy = false,
-  priority = 1000,
+  'tiagovla/tokyodark.nvim',
+  opts = {
+    -- custom options here
+    transparent_background = true,
+  },
+  config = function(_, opts)
+    require('tokyodark').setup(opts) -- calling setup is optional
+    vim.cmd [[colorscheme tokyodark]]
+  end,
 }
