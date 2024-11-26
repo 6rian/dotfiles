54a9ed8485ebccb0065315b591cce9b5368ed1fa
Brian Griffin <wbgriffin@pm.me>
2024-11-26 09:36:49 -0500

    add ts-autotag

---
+++ .config/nvim/lua/custom/plugins/ts-autotag.lua
@@ -0,0 +1,10 @@
+-- https://github.com/windwp/nvim-ts-autotag
+
+return {
+  'windwp/nvim-ts-autotag',
+  opts = {
+    enable_close = true,
+    enable_rename = true,
+    enable_close_on_slash = true,
+  },
+}
