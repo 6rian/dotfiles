caf2cb28bd57ab42bc6d00a1e41a38f1b97d206e
Brian Griffin <wbgriffin@pm.me>
2024-12-21 17:58:53 -0500

    Change lualine theme

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'ayu_dark', --'auto',
+        theme = 'auto',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
