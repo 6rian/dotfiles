d026e5e1a2d5ec633721dd61b46e8307bed22152
Brian Griffin <wbgriffin@pm.me>
2025-06-08 10:36:39 -0400

    gitsigns, guess-indent, treesitter-context

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,8 +2,12 @@ return {
   { import = 'plugins.coding.todo-comments' },
 
   { import = 'plugins.editor.completions' },
+  { import = 'plugins.editor.gitsigns' },
+  { import = 'plugins.editor.guess-indent' },
   { import = 'plugins.editor.lazygit' },
   { import = 'plugins.editor.tmux' },
+
+  { import = 'plugins.ui.treesitter-context' },
 
   { import = 'plugins.utils.which-key' },
 }
