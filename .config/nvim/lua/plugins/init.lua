ad611486f72e16ecee2e507c62e6fdcf59f44708
Brian Griffin <wbgriffin@pm.me>
2025-06-07 22:49:32 -0400

    move todo-comments and which-key

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,5 +1,9 @@
 return {
+  { import = 'plugins.coding.todo-comments' },
+
   { import = 'plugins.editor.completions' },
   { import = 'plugins.editor.lazygit' },
   { import = 'plugins.editor.tmux' },
+
+  { import = 'plugins.utils.which-key' },
 }
