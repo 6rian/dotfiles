fae208b84781429b5838eeaa48bfe831a043adf8
Brian Griffin <wbgriffin@pm.me>
2025-06-29 16:41:40 -0400

    add lualine

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -11,6 +11,7 @@ return {
   { import = 'plugins.editor.gitsigns' },
   { import = 'plugins.editor.guess-indent' },
   { import = 'plugins.editor.lazygit' },
+  { import = 'plugins.editor.lualine' },
   { import = 'plugins.editor.mini' },
   { import = 'plugins.editor.neo-tree' },
   { import = 'plugins.editor.snacks' },
