2949cb2c435ac7e0c78c5aa0276e9c4f2d43eb74
Brian Griffin <wbgriffin@pm.me>
2025-11-26 21:50:48 -0500

    increase w/h for telescope, floaterm, and lazy modals

--- .config/nvim/lua/plugins/editor/telescope.lua
+++ .config/nvim/lua/plugins/editor/telescope.lua
@@ -49,11 +49,11 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          layout_config = {
+            horizontal = { width = 0.9, height = 0.9 },
+          },
+        },
         pickers = {
           find_files = {
             find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
@@ -86,15 +86,17 @@ return {
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' }) -- Select a builtin picker
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' }) -- Resume last picker
+
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sy', builtin.symbols, { desc = '[S]earch S[y]mbols' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>so', builtin.vim_options, { desc = '[S]earch Vim [O]ptions' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
--- .config/nvim/lua/plugins/utils/vim-floaterm.lua
+++ .config/nvim/lua/plugins/utils/vim-floaterm.lua
@@ -1,7 +1,7 @@
 return {
   'voldikss/vim-floaterm',
   config = function()
-    vim.g.floaterm_width = 0.85
-    vim.g.floaterm_height = 0.85
+    vim.g.floaterm_width = 0.9
+    vim.g.floaterm_height = 0.9
   end,
 }
